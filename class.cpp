b804927130baaacc71e432382e3bc54c8df932da
phamthily <80657509+phamthily@users.noreply.github.com>
2021-11-21 15:49:35 +0700

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -73,3 +73,35 @@ struct Node{
 	Sinhvien data;
      struct Node *Next;
 };
+class List:public Sinhvien{
+	private:
+	   Node* head;
+	   Node* tail;
+	public:
+       void KhoitaoList(List &l);
+       Node* createNode(Sinhvien x);
+       void Show(List l);
+       void showNode(Node* k);
+       void input(List &l);
+       void pressAnyKey();
+       void sapxepdtl(List l);
+       void sapxepten(List l);
+       void SapXepTheoDRL(List l);
+       void timkiemten(List l);
+       void timkiemid(List l);
+       void update(List l);
+       void TimKiemSinhVienChungNganh(List l);
+       void  addhead(List &l, Node *p);
+       void addtail(List &l,Node *p);
+       void themsv(List &l);
+       void xoatheoten(List l);
+       void xoatheoID(List l);
+       void menu();
+       void Menu();
+       void SapXep(List l);
+       void TimKiem(List l);
+       void Xoa(List l);
+       void deleteList(List &l);
+       void DocFile(ifstream &filein, List &l);
+       void GhiFile(ofstream &fileout,List l);
+};
