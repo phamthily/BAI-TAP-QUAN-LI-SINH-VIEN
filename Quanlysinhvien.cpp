d41d1a752eccff83510907947b6ef9391077dd1c
phamthily <80657509+phamthily@users.noreply.github.com>
2021-12-02 23:14:04 +0700

    Update Quanlysinhvien.cpp

--- Quanlysinhvien.cpp
+++ Quanlysinhvien.cpp
@@ -186,24 +186,23 @@ void Ngaysinh:: nhapns(){
         	cout<<"Ngay sinh khong hop le. Moi ban nhap lai!!"<<endl;
 		nhapns();
     	}else{
-        	switch (thang){
-            	case 1:case 3: case 5: case 7:case 8:case 10:case 12:
+            switch (thang){
+              case 1:case 3: case 5: case 7:case 8:case 10:case 12:
              	 ngaymax=31;
              	 break;
-		case 2:
+	      case 2:
                 if((nam%4==0 && nam%100!=0)||(nam%400==0))
                     ngaymax=29;
                 else 
                     ngaymax=28;
                 break;
-		 case 4:case 6:case 9:case 11:
+	      case 4:case 6:case 9:case 11:
                 ngaymax=30;
                 break;
 	 }
         if (ngay<=ngaymax){
             cout<<"Ngay hop le !"<<endl;
-        }
-		 else{
+        } else{
             cout<<"Ngay sinh khong hop le !.Moi ban nhap lai"<<endl;
             nhapns();
         }
